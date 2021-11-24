6574e7dd1ac651d827655bea2c56390aa5b19be6
wingled22 <pelayowindel@gmail.com>
2021-11-24 15:32:29 +0800

    Add project files.

---
+++ Entities/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Wiggle.Entities
+{
+    public partial class UserType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Entities/WigglyContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Wiggle.Entities
+{
+    public partial class WigglyContext : DbContext
+    {
+        public WigglyContext()
+        {
+        }
+
+        public WigglyContext(DbContextOptions<WigglyContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<UserType> UserTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=WINSHIERO\\SQLEXPRESS;Database=Wiggly;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserType>(entity =>
+            {
+                entity.ToTable("UserType");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
